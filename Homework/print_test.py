fa3cfaa65e522e0499f23bbc06ec5b43b3f43e8f
hrandall05 <hrandall9787@floridapoly.edu>
2026-01-19 00:43:23 +0000

    L01HW

---
+++ Homework/print_test.py
@@ -0,0 +1,8 @@
+
+
+my_name = "Hunter Randall"
+print(my_name)
+print("Use 4 spaces per indentation level.")
+print("Python uses indentation to define code blocks rather than curly brackets({})")
+print("By Taking this class, I hope to deepen my understanding of python," \
+      " become more effecient problem solver, and increase my coding speed.")
